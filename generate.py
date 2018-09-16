423f1df93c493ac586014d65c73eb6b000617d99
Carl Simon Adorf <csadorf@umich.edu>
2018-09-16 15:27:01 -0400

    Refactor code for style including adherence to PEP8.

--- generate.py
+++ generate.py
@@ -8,37 +8,39 @@ hoomd.context.initialize('--mode=cpu')
 n = 5        # number of vertices
 seed = 1     # RNG seed
 
+
 def get_area(vertices):
-    """ Compute area of polygon """
-    n = len(vertices);
-    a = 0.0;
+    """Compute area of polygon."""
+    n = len(vertices)
+    a = 0.0
     for i in range(n):
-        j = (i + 1) % n;
-        a += abs(vertices[i][0] * vertices[j][1]-vertices[j][0] * vertices[i][1]);
-    result = a / 2.0;
-    return result;
+        j = (i + 1) % n
+        a += abs(vertices[i][0] * vertices[j][1]-vertices[j][0] * vertices[i][1])
+    return a / 2.0
+
 
 def get_vertices(n):
-    """ Compute vertices of a regular n-gon """
-    theta = numpy.linspace(0, 2*math.pi, num=n, endpoint=False);
-    return 0.5*(numpy.array([numpy.cos(theta), numpy.sin(theta)]).T);
+    """Compute vertices of a regular n-gon."""
+    theta = numpy.linspace(0, 2*math.pi, num=n, endpoint=False)
+    return 0.5 * numpy.array([numpy.cos(theta), numpy.sin(theta)]).T
+
 
 # derived parameters
-vertices = get_vertices(n);
-poly_area = get_area(vertices);
+vertices = get_vertices(n)
+poly_area = get_area(vertices)
 
 # compute box parameters
-m = 20;
+m = 20
 phi = 0.6   # packing fraction (for initial config and move size tuning)
-cell_area = 2 * poly_area / phi;
+cell_area = 2 * poly_area / phi
 a = math.sqrt(cell_area / math.sqrt(3))
 
 # initialize system
-hoomd.init.create_lattice(unitcell=hoomd.lattice.hex(a=a), n=[m, int(m/math.sqrt(3))]);
-mc = hoomd.hpmc.integrate.convex_polygon(d=0.2, a=0.2, seed=seed);
-mc.shape_param.set('A', vertices=vertices);
+hoomd.init.create_lattice(unitcell=hoomd.lattice.hex(a=a), n=[m, int(m/math.sqrt(3))])
+mc = hoomd.hpmc.integrate.convex_polygon(d=0.2, a=0.2, seed=seed)
+mc.shape_param.set('A', vertices=vertices)
 
 # write out generated file
-d = hoomd.dump.gsd("init.gsd", period=1, group=hoomd.group.all(), overwrite=True);
-d.dump_state(mc);
-d.write_restart();
+d = hoomd.dump.gsd("init.gsd", period=1, group=hoomd.group.all(), overwrite=True)
+d.dump_state(mc)
+d.write_restart()
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import hoomd
 import hoomd.hpmc
+
 hoomd.context.initialize('--mode=cpu')
 
 # parameters
@@ -7,17 +8,17 @@ seed = 1     # RNG seed
 P = 13.2    # pressure
 
 # run the simulation
-hoomd.init.read_gsd(filename='init.gsd', restart='restart.gsd', time_step=0);
-mc = hoomd.hpmc.integrate.convex_polygon(restore_state=True, seed=seed);
-d = hoomd.dump.gsd("trajectory.gsd", period=500, group=hoomd.group.all());
-d.dump_state(mc);
+hoomd.init.read_gsd(filename='init.gsd', restart='restart.gsd', time_step=0)
+mc = hoomd.hpmc.integrate.convex_polygon(restore_state=True, seed=seed)
+d = hoomd.dump.gsd("trajectory.gsd", period=500, group=hoomd.group.all())
+d.dump_state(mc)
 
-restart = hoomd.dump.gsd("restart.gsd", period=500, group=hoomd.group.all(), truncate=True);
-restart.dump_state(mc);
+restart = hoomd.dump.gsd("restart.gsd", period=500, group=hoomd.group.all(), truncate=True)
+restart.dump_state(mc)
 
-hoomd.analyze.log(filename="log.dat", quantities=['volume'], period=100);
-boxmc = hoomd.hpmc.update.boxmc(mc, betaP=P, seed=seed);
-boxmc.ln_volume(delta=0.001,weight=1);
+hoomd.analyze.log(filename="log.dat", quantities=['volume'], period=100)
+boxmc = hoomd.hpmc.update.boxmc(mc, betaP=P, seed=seed)
+boxmc.ln_volume(delta=0.001, weight=1)
 
-hoomd.run(20000);
-restart.write_restart();
+hoomd.run(20000)
+restart.write_restart()
